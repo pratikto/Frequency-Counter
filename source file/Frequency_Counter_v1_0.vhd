3d3760276e109786c6d1baad770b4a88b4306545
Pratikto <pratikto.sulthoni.h@gmail.com>
2020-09-01 20:46:38 +0700

    insert Design wrapper to AXI lite

--- source file/Frequency_Counter_v1_0.vhd
+++ source file/Frequency_Counter_v1_0.vhd
@@ -12,11 +12,26 @@ entity Frequency_Counter_v1_0 is
 
 		-- Parameters of Axi Slave Bus Interface S00_AXI
 		C_S00_AXI_DATA_WIDTH	: integer	:= 32;
-		C_S00_AXI_ADDR_WIDTH	: integer	:= 4
+		C_S00_AXI_ADDR_WIDTH	: integer	:= 4;
+
+		-- Parameters of Axi Slave Bus Interface S_AXI_INTR
+		C_S_AXI_INTR_DATA_WIDTH	: integer	:= 32;
+		C_S_AXI_INTR_ADDR_WIDTH	: integer	:= 5;
+		C_NUM_OF_INTR	: integer	:= 1;
+		C_INTR_SENSITIVITY	: std_logic_vector	:= x"FFFFFFFF";
+		C_INTR_ACTIVE_STATE	: std_logic_vector	:= x"FFFFFFFF";
+		C_IRQ_SENSITIVITY	: integer	:= 1;
+		C_IRQ_ACTIVE_STATE	: integer	:= 1
 	);
 	port (
 		-- Users to add ports here
-
+        Freq : out STD_LOGIC_VECTOR ( 63 downto 0 );
+        SCLR : in STD_LOGIC;
+        clk : in STD_LOGIC;
+        error : out STD_LOGIC;
+        gate : in STD_LOGIC;
+        inputSignal : in STD_LOGIC;
+        ready : out STD_LOGIC;
 		-- User ports ends
 		-- Do not modify the ports beyond this line
 
@@ -42,7 +57,31 @@ entity Frequency_Counter_v1_0 is
 		s00_axi_rdata	: out std_logic_vector(C_S00_AXI_DATA_WIDTH-1 downto 0);
 		s00_axi_rresp	: out std_logic_vector(1 downto 0);
 		s00_axi_rvalid	: out std_logic;
-		s00_axi_rready	: in std_logic
+		s00_axi_rready	: in std_logic;
+
+		-- Ports of Axi Slave Bus Interface S_AXI_INTR
+		s_axi_intr_aclk	: in std_logic;
+		s_axi_intr_aresetn	: in std_logic;
+		s_axi_intr_awaddr	: in std_logic_vector(C_S_AXI_INTR_ADDR_WIDTH-1 downto 0);
+		s_axi_intr_awprot	: in std_logic_vector(2 downto 0);
+		s_axi_intr_awvalid	: in std_logic;
+		s_axi_intr_awready	: out std_logic;
+		s_axi_intr_wdata	: in std_logic_vector(C_S_AXI_INTR_DATA_WIDTH-1 downto 0);
+		s_axi_intr_wstrb	: in std_logic_vector((C_S_AXI_INTR_DATA_WIDTH/8)-1 downto 0);
+		s_axi_intr_wvalid	: in std_logic;
+		s_axi_intr_wready	: out std_logic;
+		s_axi_intr_bresp	: out std_logic_vector(1 downto 0);
+		s_axi_intr_bvalid	: out std_logic;
+		s_axi_intr_bready	: in std_logic;
+		s_axi_intr_araddr	: in std_logic_vector(C_S_AXI_INTR_ADDR_WIDTH-1 downto 0);
+		s_axi_intr_arprot	: in std_logic_vector(2 downto 0);
+		s_axi_intr_arvalid	: in std_logic;
+		s_axi_intr_arready	: out std_logic;
+		s_axi_intr_rdata	: out std_logic_vector(C_S_AXI_INTR_DATA_WIDTH-1 downto 0);
+		s_axi_intr_rresp	: out std_logic_vector(1 downto 0);
+		s_axi_intr_rvalid	: out std_logic;
+		s_axi_intr_rready	: in std_logic;
+		irq	: out std_logic
 	);
 end Frequency_Counter_v1_0;
 
@@ -79,6 +118,54 @@ architecture arch_imp of Frequency_Counter_v1_0 is
 		);
 	end component Frequency_Counter_v1_0_S00_AXI;
 
+	component Frequency_Counter_v1_0_S_AXI_INTR is
+		generic (
+		C_S_AXI_DATA_WIDTH	: integer	:= 32;
+		C_S_AXI_ADDR_WIDTH	: integer	:= 5;
+		C_NUM_OF_INTR	: integer	:= 1;
+		C_INTR_SENSITIVITY	: std_logic_vector	:= x"FFFFFFFF";
+		C_INTR_ACTIVE_STATE	: std_logic_vector	:= x"FFFFFFFF";
+		C_IRQ_SENSITIVITY	: integer	:= 1;
+		C_IRQ_ACTIVE_STATE	: integer	:= 1
+		);
+		port (
+		S_AXI_ACLK	: in std_logic;
+		S_AXI_ARESETN	: in std_logic;
+		S_AXI_AWADDR	: in std_logic_vector(C_S_AXI_ADDR_WIDTH-1 downto 0);
+		S_AXI_AWPROT	: in std_logic_vector(2 downto 0);
+		S_AXI_AWVALID	: in std_logic;
+		S_AXI_AWREADY	: out std_logic;
+		S_AXI_WDATA	: in std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0);
+		S_AXI_WSTRB	: in std_logic_vector((C_S_AXI_DATA_WIDTH/8)-1 downto 0);
+		S_AXI_WVALID	: in std_logic;
+		S_AXI_WREADY	: out std_logic;
+		S_AXI_BRESP	: out std_logic_vector(1 downto 0);
+		S_AXI_BVALID	: out std_logic;
+		S_AXI_BREADY	: in std_logic;
+		S_AXI_ARADDR	: in std_logic_vector(C_S_AXI_ADDR_WIDTH-1 downto 0);
+		S_AXI_ARPROT	: in std_logic_vector(2 downto 0);
+		S_AXI_ARVALID	: in std_logic;
+		S_AXI_ARREADY	: out std_logic;
+		S_AXI_RDATA	: out std_logic_vector(C_S_AXI_DATA_WIDTH-1 downto 0);
+		S_AXI_RRESP	: out std_logic_vector(1 downto 0);
+		S_AXI_RVALID	: out std_logic;
+		S_AXI_RREADY	: in std_logic;
+		irq	: out std_logic
+		);
+	end component Frequency_Counter_v1_0_S_AXI_INTR;
+	
+	component FreqCounterdesign_wrapper
+    port(
+        Freq : out STD_LOGIC_VECTOR ( 63 downto 0 );
+        SCLR : in STD_LOGIC;
+        clk : in STD_LOGIC;
+        error : out STD_LOGIC;
+        gate : in STD_LOGIC;
+        inputSignal : in STD_LOGIC;
+        ready : out STD_LOGIC
+    );
+    end component;
+
 begin
 
 -- Instantiation of Axi Bus Interface S00_AXI
@@ -111,8 +198,52 @@ Frequency_Counter_v1_0_S00_AXI_inst : Frequency_Counter_v1_0_S00_AXI
 		S_AXI_RREADY	=> s00_axi_rready
 	);
 
-	-- Add user logic here
+-- Instantiation of Axi Bus Interface S_AXI_INTR
+Frequency_Counter_v1_0_S_AXI_INTR_inst : Frequency_Counter_v1_0_S_AXI_INTR
+	generic map (
+		C_S_AXI_DATA_WIDTH	=> C_S_AXI_INTR_DATA_WIDTH,
+		C_S_AXI_ADDR_WIDTH	=> C_S_AXI_INTR_ADDR_WIDTH,
+		C_NUM_OF_INTR	=> C_NUM_OF_INTR,
+		C_INTR_SENSITIVITY	=> C_INTR_SENSITIVITY,
+		C_INTR_ACTIVE_STATE	=> C_INTR_ACTIVE_STATE,
+		C_IRQ_SENSITIVITY	=> C_IRQ_SENSITIVITY,
+		C_IRQ_ACTIVE_STATE	=> C_IRQ_ACTIVE_STATE
+	)
+	port map (
+		S_AXI_ACLK	=> s_axi_intr_aclk,
+		S_AXI_ARESETN	=> s_axi_intr_aresetn,
+		S_AXI_AWADDR	=> s_axi_intr_awaddr,
+		S_AXI_AWPROT	=> s_axi_intr_awprot,
+		S_AXI_AWVALID	=> s_axi_intr_awvalid,
+		S_AXI_AWREADY	=> s_axi_intr_awready,
+		S_AXI_WDATA	=> s_axi_intr_wdata,
+		S_AXI_WSTRB	=> s_axi_intr_wstrb,
+		S_AXI_WVALID	=> s_axi_intr_wvalid,
+		S_AXI_WREADY	=> s_axi_intr_wready,
+		S_AXI_BRESP	=> s_axi_intr_bresp,
+		S_AXI_BVALID	=> s_axi_intr_bvalid,
+		S_AXI_BREADY	=> s_axi_intr_bready,
+		S_AXI_ARADDR	=> s_axi_intr_araddr,
+		S_AXI_ARPROT	=> s_axi_intr_arprot,
+		S_AXI_ARVALID	=> s_axi_intr_arvalid,
+		S_AXI_ARREADY	=> s_axi_intr_arready,
+		S_AXI_RDATA	=> s_axi_intr_rdata,
+		S_AXI_RRESP	=> s_axi_intr_rresp,
+		S_AXI_RVALID	=> s_axi_intr_rvalid,
+		S_AXI_RREADY	=> s_axi_intr_rready,
+		irq	=> irq
+	);
 
+	-- Add user logic here
+    FreqCounter : FreqCounterdesign_wrapper port map(
+        Freq => Freq,
+        SCLR => SCLR,
+        clk => clk,
+        error => error,
+        gate => gate,
+        inputSignal => inputSignal,
+        ready => ready
+    );
 	-- User logic ends
 
 end arch_imp;
